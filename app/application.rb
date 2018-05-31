dc0ad986b5ddb260cbab816c8012c6dc20d2aef6
Spencer Goodman <spencerwgoodman@icloud.com>
2018-05-31 00:45:07 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -11,6 +11,12 @@ class Application
       @@items.each do |item|
         resp.write "#{item}\n"
       end
+    elsif req.path.match(/cart/)
+      if @@cart = []
+        resp.write "Your cart is empty"
+      else
+        @@cart
+      end
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
