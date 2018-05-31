b55dadd2a18353769b5234641540b7ad7228ffc9
Spencer Goodman <spencerwgoodman@icloud.com>
2018-05-31 00:54:12 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -22,7 +22,6 @@ class Application
     elsif req.path.match(/add/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
-      binding.pry
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
